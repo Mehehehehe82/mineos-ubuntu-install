30d4b4863d9f9f9899f0d6789e19e6bf6e23b929
Tem326 <evanpbishop@Gmail.com>
2019-10-09 03:31:36 -0400

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,12 @@
 # run as root
+mkdir -p /usr/games
+cd /usr/games
+git clone https://github.com/hexparrot/mineos-node.git minecraft
+cd minecraft
+git config core.filemode false
+chmod +x service.js mineos_console.js generate-sslcert.sh webui.js
+npm install --unsafe-perm
+ln -s /usr/games/minecraft/mineos_console.js /usr/local/bin/mineos
+
+
+cp mineos.conf /etc/mineos.conf
